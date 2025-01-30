c18c0bb0d6f8fe42752083d710f1d3d7f35b7878
AnthonyOfTulsa <AnthonyOfTulsa@gmail.com>
2025-01-30 10:12:09 -0600

    swerve: Updating whitespace
    WPI compile likes to reformat lines.

--- src/main/java/frc/robot/subsystems/drive/DriveConstants.java
+++ src/main/java/frc/robot/subsystems/drive/DriveConstants.java
@@ -90,8 +90,10 @@ public class DriveConstants {
 
   // Turn encoder configuration
   public static final boolean turnEncoderInverted = false;
-  public static final double turnEncoderPositionFactor = 2 * Math.PI / turnMotorReduction; // Rotations -> Radians
-  public static final double turnEncoderVelocityFactor = (2 * Math.PI) / 60.0 / turnMotorReduction; // RPM -> Rad/Sec
+  public static final double turnEncoderPositionFactor =
+      2 * Math.PI / turnMotorReduction; // Rotations -> Radians
+  public static final double turnEncoderVelocityFactor =
+      (2 * Math.PI) / 60.0 / turnMotorReduction; // RPM -> Rad/Sec
 
   // CANcoder offsets from programming bot
   // TODO: Change CANcoder offsets to values for competition bot
