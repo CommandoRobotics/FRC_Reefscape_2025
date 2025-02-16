d0ec2999d92f15cf99e30f147172d0120971aabc
mason <93228033+MasonI2006@users.noreply.github.com>
2025-02-15 18:30:20 -0600

    Added hand subsystem. Controls wrist motion with PID and controls expelling/uptaking coral based off a beam break. added basic commands

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FRC_Reefscape_2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 10;
-  public static final String GIT_SHA = "c18c0bb0d6f8fe42752083d710f1d3d7f35b7878";
-  public static final String GIT_DATE = "2025-01-30 11:12:09 EST";
-  public static final String GIT_BRANCH = "Mason_Elevator";
-  public static final String BUILD_DATE = "2025-02-11 18:57:44 EST";
-  public static final long BUILD_UNIX_TIME = 1739318264134L;
-  public static final int DIRTY = 1;
+  public static final int GIT_REVISION = 13;
+  public static final String GIT_SHA = "06fcef84085d111d6eabee3ffe28cc269881d487";
+  public static final String GIT_DATE = "2025-02-13 17:41:37 EST";
+  public static final String GIT_BRANCH = "Mason_Hand";
+  public static final String BUILD_DATE = "2025-02-15 19:01:12 EST";
+  public static final long BUILD_UNIX_TIME = 1739664072364L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
