2acaf4164b3a9e195a57a852c3150883f711498f
CasciaRobots <CasciaRobots@gmail.com>
2025-02-22 09:42:48 -0600

    debugging advantage swerve

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -3,15 +3,15 @@ package frc.robot;
 /** Automatically generated file containing build version information. */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "AdvantageKit_SparkSwerveTemplate";
+  public static final String MAVEN_NAME = "FRC_Reefscape_2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = -1;
-  public static final String GIT_SHA = "UNKNOWN";
-  public static final String GIT_DATE = "UNKNOWN";
-  public static final String GIT_BRANCH = "UNKNOWN";
-  public static final String BUILD_DATE = "2025-01-10 17:31:24 EST";
-  public static final long BUILD_UNIX_TIME = 1736548284938L;
-  public static final int DIRTY = 129;
+  public static final int GIT_REVISION = 6;
+  public static final String GIT_SHA = "3227c9d3c4493e9f886637f3b11b42506b4d89f5";
+  public static final String GIT_DATE = "2025-01-30 10:58:09 EST";
+  public static final String GIT_BRANCH = "mason_swerve_advantage";
+  public static final String BUILD_DATE = "2025-02-11 19:50:47 EST";
+  public static final long BUILD_UNIX_TIME = 1739321447270L;
+  public static final int DIRTY = 1;
 
   private BuildConstants() {}
 }
