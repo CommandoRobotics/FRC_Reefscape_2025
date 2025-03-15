fbd871b244123bf41998d9c52670013a0277fa95
mason <93228033+MasonI2006@users.noreply.github.com>
2025-03-14 22:05:58 -0500

    day 2 changes

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FRC_Reefscape_2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 40;
-  public static final String GIT_SHA = "da09497f552780a322b01d0ff0cd99347818476e";
-  public static final String GIT_DATE = "2025-03-14 11:10:17 EDT";
+  public static final int GIT_REVISION = 41;
+  public static final String GIT_SHA = "63c7af04f57a122dfb89f5963cd075f1432364e8";
+  public static final String GIT_DATE = "2025-03-14 17:13:04 EDT";
   public static final String GIT_BRANCH = "masonDevelop";
-  public static final String BUILD_DATE = "2025-03-14 17:01:38 EDT";
-  public static final long BUILD_UNIX_TIME = 1741986098819L;
+  public static final String BUILD_DATE = "2025-03-14 17:47:16 EDT";
+  public static final long BUILD_UNIX_TIME = 1741988836368L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
